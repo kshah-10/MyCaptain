bada5925a1c89eaa8527ecf662e205b7600908c1
kshah-10 <ENG21CT0015@dsu.edu.in>
2023-08-20 21:35:15 +0530

    Initial Commit

---
+++ Sets.py
@@ -0,0 +1,10 @@
+E = {2,4,15,27,28,7,17};
+N = {3,5,7,11,12,2};
+
+print("Union of E and N is",E | N)
+
+print("Intersection of E and N is",E & N)
+
+print("Difference of E and N is",E - N)
+
+print("Symmetric difference of E and N is",E ^ N)
